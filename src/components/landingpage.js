405af982cb4da855c1c35b2f5aebe724ef977588
Connie Huynh <connie.huynhh@gmail.com>
2019-09-22 02:21:26 -0700

    Profile
    Profile mockup, working nav bar, splash page, etc.

---
+++ src/components/landingpage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class LandingPage extends Component{
+    render(){
+        return(
+            <div style={{width:'100%', margin: 'auto'}}> 
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                        src="http://i1.hdslb.com/bfs/archive/ba5cfd393d15275c9a7cbc1f42ee88747b0136bb.jpg"
+                        alt="logo"
+                        className="logo-image"
+                        />
+
+                        <div className="banner">
+                            <h1>Flume Rides</h1>
+                        <hr/>
+                        <p>meow meow meow meow | bark | test</p>
+                        <div className="drive-links">
+                            {/* Drive */}
+                            <a href="/drive" rel ="noopener noreferrer">
+                                <i className="fas fa-car"  aria-hidden="true"/>
+                            </a>
+                            
+                            {/* Drive */}
+                            <a href="/ride" rel ="noopener noreferrer">
+                                <i className="fas fa-users"  aria-hidden="true"/>
+                            </a>                            
+
+                        </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+        )
+    }
+}
+export default LandingPage;
---
+++ src/components/ride.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+
+class Ride extends Component{
+    constructor(props){
+        super(props);
+        this.state ={ activeTab: 0};
+    }
+    toggleCategories(){
+        {/* Ride 1 */}
+        if(this.state.activeTab === 0){
+            return(
+                <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', background: 'Url(https://vignette.wikia.nocookie.net/webarebears/images/3/37/Ice_bear.png/revision/latest/scale-to-width-down/2000?cb=20160619204008) center / cover'}}>Ride 1</CardTitle>
+                    <CardText>
+                        nom nom nom nom
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Accept</Button>
+                        <Button colored>Decline</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
+            )
+        }
+
+        else if(this.state.activeTab === 1){
+            return(
+                <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', background: 'Url(https://www.stickpng.com/assets/images/5c17909a44f5fd02b8cd3b74.png) center / cover'}}>Ride 2</CardTitle>
+                    <CardText>
+                        nom nom nom nom
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Accept</Button>
+                        <Button colored>Decline</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
+            )
+        }
+        
+        else if(this.state.activeTab === 2){
+            return(
+                <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', background: 'Url(https://p7.hiclipart.com/preview/411/606/476/giant-panda-polar-bear-stirfry-stunts-we-bare-bears-grizzly-bear-polar-bear.jpg) center / cover'}}>Ride 3</CardTitle>
+                    <CardText>
+                        nom nom nom nom
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Accept</Button>
+                        <Button colored>Decline</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
+            )
+        }
+    }
+
+    
+    render(){
+        return(
+            <div className="categories">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>Ride1</Tab>
+                    <Tab>Ride2</Tab>
+                    <Tab>Ride3</Tab>
+                </Tabs>
+                <section className="rides-grid">
+                    <Grid className="rides-grid">
+                        <Cell col={12}>
+                            {this.toggleCategories()}
+                        </Cell>
+                    </Grid>
+                </section>
+            </div>
+        )
+    }
+}
+export default Ride;
