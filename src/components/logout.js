966bd9cf7be59a5c6d4117b0b7813c22c395c75c
Connie Huynh <connie.huynh@sjsu.edu>
2019-10-23 10:40:37 -0700

    Add Forms, Integrate Search Bar, Update Pages

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Logout extends Component{
     render(){
         return(
-            <div><h1>Logout Page</h1></div>
+            <div className="logout-body">
+                <Grid className="logout-grid">
+                    <h1>Logout</h1>
+                </Grid>
+            </div>
         )
     }
 }
