91ed37e55650c626d7176d6c896de89f831027d2
Brandon Archbold <babrandon21@gmail.com>
2019-11-14 22:34:44 -0800

    moved routes to a separate file

---
+++ server/routes/routes.js
@@ -0,0 +1,39 @@
+module.exports = function(app, connection) {
+    app.get('/', (req, res) => {
+        connection.query('SELECT * FROM users', function (err, data) {
+            (err)?res.send(err):res.json({users: data});
+        });
+    });
+    
+    app.get('/login_verify', (req, res) => {
+        email = req.body.email;
+        password = req.body.password
+    
+        connection.query(query, (err, result) => {
+            "SELECT name FROM `users` WHERE `email` = '" + email +  "', passwordHash = '" + password + "'; ";
+            if(err) {
+                console.log(err);
+            }
+            console.log('successfully queried');
+        });
+    });   
+    
+    app.post('/new_user', (req, res) => {
+        console.log('in post');
+        let name = req.body.user.name;
+        let email = req.body.user.email;
+        let school = req.body.user.school;
+        let password = req.body.password;
+    
+        console.log('name: ' + name + ', email: ' + email);
+    
+        let query = "INSERT INTO `users` (`name`, `email`, `school`, `passwordHash`) VALUES ('" + name + "', '" + email + "', '" + school + "', '" + password + "');";
+    
+        connection.query(query, (err, result) => {
+            if(err){
+                console.log(err);
+            } 
+            console.log('entry added, result was ' + result);
+        });
+    });
+}
