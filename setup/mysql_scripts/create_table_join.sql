04b4472519456d5d3a5961efaf74c60149b37846
JohnPatMcGinley <36456455+JohnPatMcGinley@users.noreply.github.com>
2019-11-25 11:40:46 -0800

    Fixed MySQL naming conflicts

--- setup/mysql_scripts/create_table_join.sql
+++ setup/mysql_scripts/create_table_join.sql
@@ -1,18 +1,21 @@
-CREATE TABLE `join` (
+DROP TABLE IF EXISTS `join`;
+DROP TABLE IF EXISTS `join_ride`;
+
+CREATE TABLE `join_ride` (
     `email` VARCHAR(50) NOT NULL,
     `datetime` DATETIME NOT NULL,
 
     PRIMARY KEY (`email`, `datetime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-LOCK TABLES `join` WRITE;
+LOCK TABLES `join_ride` WRITE;
 
 --
--- Dumping data for table `join`
+-- Dumping data for table `join_ride`
 --
 
-INSERT INTO `join` VALUES ('BA@gmail.com', '2019-11-4 12:34:21'),
-('ConnieH@gmail.com', '2019-11-4 04:18:39'),
-('John.McGinley@sjsu.edu', '2019-11-4 08:23:34');
+INSERT INTO `join_ride` VALUES ('BA@gmail.com', '2019-11-4 12:34:21', 'ConnieH@gmail.com'),
+('ConnieH@gmail.com', '2019-11-4 04:18:39', 'John.McGinley@SJSU.edu'),
+('John.McGinley@sjsu.edu', '2019-11-4 08:23:34', 'ConnieH@gmail.com');
 
 UNLOCK TABLES;
